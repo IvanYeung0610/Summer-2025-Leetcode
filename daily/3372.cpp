50219fef5d3127c404e74ccf88b557fa0b26c2cf
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-05-31 18:03:13 -0400

    started on 3372

---
+++ daily/3372.cpp
@@ -0,0 +1,17 @@
+// Ivan Yeung
+// 3372. Maximize the Number of Target Nodes after Connecting Trees I
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+	}
+};
+
+int main() {
+	return 0;
+}
