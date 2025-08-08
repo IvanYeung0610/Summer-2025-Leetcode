960abcce8d4e6e6a7483b960081fd477e43f3190
Ivan Yeung <ivanyeung0610@gmail.com>
2025-08-08 14:38:48 -0400

    Finished 110.cpp

--- neetcode/trees/110.cpp
+++ neetcode/trees/110.cpp
@@ -1,6 +1,10 @@
 // Ivan Yeung
 // 110. Balanced Binary Tree
 
+#include <vector>
+
+using namespace std;
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -24,6 +28,23 @@ struct TreeNode {
 
 class Solution {
 public:
+    vector<int> dfs(TreeNode* root) {
+        // returns 2 element vector: (tree balanced?, depth)
+        if (!root) return {0,0};
+
+        vector<int> left = dfs(root->left);
+        vector<int> right = dfs(root->right);
+        
+        // checking if the depth of the 2 subtrees differ by more than 1
+        if (left[1] == right[1] || left[1] == right[1] - 1 
+            || left[1] - 1 == right[1]) {
+            return {max(left[0], right[0]), max(left[1], right[1]) + 1};
+        } else {
+            return {1, max(left[1], right[1]) + 1};
+        }
+    }
+
     bool isBalanced(TreeNode* root) {
+        return !dfs(root)[0];
     }
 };
